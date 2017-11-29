9d0b6e018b0778a9225a6d223d1fc317e2de4e9e
Afri Schoedon <5chdn@users.noreply.github.com>
2017-11-29 11:32:35 +0100

    Fix the License to be consistently APACHE 2.0 across all contracts. (#97)
    * Fix LICENSE GPLv3 -> APLv2
    
    * Prepend solidity headers with correct APLv2 license and copyright information.

--- Operations.sol
+++ Operations.sol
@@ -1,6 +1,18 @@
-// Operations contract, by Gavin Wood.
-// Copyright Parity Technologies Ltd (UK), 2016.
-// This code may be distributed under the terms of the Apache Licence, version 2.
+//! The operations contract.
+//!
+//! Copyright 2016 Gavin Wood, Parity Technologies Ltd.
+//!
+//! Licensed under the Apache License, Version 2.0 (the "License");
+//! you may not use this file except in compliance with the License.
+//! You may obtain a copy of the License at
+//!
+//!     http://www.apache.org/licenses/LICENSE-2.0
+//!
+//! Unless required by applicable law or agreed to in writing, software
+//! distributed under the License is distributed on an "AS IS" BASIS,
+//! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//! See the License for the specific language governing permissions and
+//! limitations under the License.
 
 pragma solidity ^0.4.7;
 
