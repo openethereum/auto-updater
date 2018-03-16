f936e1a3cafeb6034caf0867ce4fb07ed4ee3f55
André Silva <andre.beat@gmail.com>
2018-03-16 22:06:12 +0000

    SimpleOperations: test fallback function

--- test/simple_operations.js
+++ test/simple_operations.js
@@ -9,7 +9,26 @@ contract("SimpleOperations", accounts => {
 
     assert.equal(owner, accounts[0]);
 
+    // the creator of the operations contract should be set as the owner of the parity client
     let client = await operations.clientOwner.call(accounts[0]);
     assert.equal(web3.toUtf8(client), "parity");
+  });
+
+  it("should emit a `Received` event on fallback", async () => {
+    let operations = await SimpleOperations.deployed();
+    let watcher = operations.Received();
+
+    await operations.sendTransaction({
+      from: accounts[1],
+      value: 3,
+      data: web3.fromUtf8("hello"),
+    });
+
+    let events = await watcher.get();
+
+    assert.equal(events.length, 1);
+    assert.equal(events[0].args.from, accounts[1]);
+    assert.equal(events[0].args.value.valueOf(), 3);
+    assert.equal(web3.toUtf8(events[0].args.data), "hello");
   });
 });
