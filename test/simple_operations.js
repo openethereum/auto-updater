12584b68d4e596a1dc7f159f2e3959197610997c
André Silva <andre.beat@gmail.com>
2018-03-16 14:11:45 +0000

    fix tests

--- test/simple_operations.js
+++ test/simple_operations.js
@@ -5,10 +5,9 @@ let SimpleOperations = artifacts.require("./SimpleOperations.sol");
 contract("SimpleOperations", accounts => {
   it("should initialize the contract with the parity client", async () => {
     let operations = await SimpleOperations.deployed();
-    let [owner, required] = await operations.client.call("parity");
+    let owner = await operations.client.call("parity");
 
     assert.equal(owner, accounts[0]);
-    assert(required);
 
     let client = await operations.clientOwner.call(accounts[0]);
     assert.equal(web3.toUtf8(client), "parity");
