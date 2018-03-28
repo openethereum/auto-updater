fcaad80cd94a6bcbb5443cb4ca7bc91664797d7c
André Silva <andre.beat@gmail.com>
2018-03-28 16:59:37 +0100

    add braces to all if statements

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -6,8 +6,9 @@ contract Migrations {
 	uint public last_completed_migration;
 
 	modifier restricted() {
-		if (msg.sender == owner)
+		if (msg.sender == owner) {
 			_;
+		}
 	}
 
 	function Migrations() public {
