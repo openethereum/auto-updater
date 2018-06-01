adaf99a5306a27b00fb237a84c47603c6d8f53ab
André Silva <andre.beat@gmail.com>
2018-06-01 13:05:42 +0100

    update dependencies and solidity pragmas

--- contracts/SimpleOperations.sol
+++ contracts/SimpleOperations.sol
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.22;
 
 import "./Operations.sol";
 
