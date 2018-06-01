c94165e706e6fec8f922efcfd88da18db50baa92
André Silva <andre.beat@gmail.com>
2018-06-01 14:31:41 +0100

    define operations as interface

--- contracts/Operations.sol
+++ contracts/Operations.sol
@@ -17,7 +17,7 @@
 pragma solidity ^0.4.22;
 
 
-contract Operations {
+interface Operations {
 	/// Tracks
 	// STABLE = 1;
 	// BETA = 2;
@@ -47,36 +47,36 @@ contract Operations {
 		uint24 _semver,
 		bool _critical
 	)
-		public;
+		external;
 
 	function addChecksum(bytes32 _release, bytes32 _platform, bytes32 _checksum)
-		public;
+		external;
 
 	function setClientOwner(address _newOwner)
-		public;
+		external;
 
 	function isLatest(bytes32 _client, bytes32 _release)
-		public
+		external
 		view
 		returns (bool);
 
 	function track(bytes32 _client, bytes32 _release)
-		public
+		external
 		view
 		returns (uint8);
 
 	function latestInTrack(bytes32 _client, uint8 _track)
-		public
+		external
 		view
 		returns (bytes32);
 
 	function build(bytes32 _client, bytes32 _checksum)
-		public
+		external
 		view
 		returns (bytes32 o_release, bytes32 o_platform);
 
 	function release(bytes32 _client, bytes32 _release)
-		public
+		external
 		view
 		returns (
 			uint32 o_forkBlock,
@@ -86,17 +86,17 @@ contract Operations {
 		);
 
 	function checksum(bytes32 _client, bytes32 _release, bytes32 _platform)
-		public
+		external
 		view
 		returns (bytes32);
 
 	function latestFork()
-		public
+		external
 		view
 		returns (uint32);
 
 	function clientOwner(address _owner)
-		public
+		external
 		view
 		returns (bytes32);
 }
